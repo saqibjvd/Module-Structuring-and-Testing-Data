92bd64935b2a90852b51d3f22e53e5b99401659e
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2024-10-29 17:22:24 +0000

    Sprint-1
    exercises count and decimal done.

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -3,7 +3,25 @@ const num = 56.5678;
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
+const wholeNumberPart = Math.trunc(num)
+console.log(wholeNumberPart) // Output: 56
+
 // Create a variable called decimalPart and assign to it an expression that evaluates to 0.5678 ( the decimal part of num )
+const decimalPart = num - Math.floor(num);
+console.log(decimalPart); // Output: 0.5678
+
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
+const roundedNum = Math.ceil(num)
+console.log(roundedNum) // Output: 57
+
 
 // Log your variables to the console to check your answers
+
+
+
+
+
+
+
+
+
