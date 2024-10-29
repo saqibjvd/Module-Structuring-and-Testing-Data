42d25fd6a1fc7af7d08d1bad5b4200a0a3241eed
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2024-10-29 17:31:22 +0000

    Sprint-1
    Exercises: count and decimals completed

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -1,6 +1,8 @@
 let count = 0;
 
 count = count + 1;
+console.log(count)  
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// on line 3 its updating value of count from 0 to 1. 
